f2af232736af1ce0c345dee8ef174df64222c4c9
Jan Christoph Terasa <christoph@kohlio.de>
2024-12-01 19:24:00 +0100

    Simplify day 1

--- python/day01.py
+++ python/day01.py
@@ -2,23 +2,22 @@
 
 from collections import Counter
 
-from puzzle_input_getter import get_puzzle_input_tee
+from puzzle_input_getter import get_puzzle_input
 
 
-def part1(input):
-    first, second = zip(*((int(n) for n in line.split()) for line in input))
+def part1(first, second):
     diff = (abs(f - s) for f, s in zip(sorted(first), sorted(second)))
     return sum(diff)
 
 
-def part2(input):
-    first, second = zip(*((int(n) for n in line.split()) for line in input))
+def part2(first, second):
     second_counts = Counter(second)
     similarity = (f * second_counts[f] for f in first if f in second_counts)
     return sum(similarity)
 
 
 if __name__ == "__main__":
-    p1, p2 = get_puzzle_input_tee(year=2024, day=1)
-    print(f"part1: {part1(p1)}")
-    print(f"part2: {part2(p2)}")
+    input = get_puzzle_input(year=2024, day=1)
+    first, second = tuple(zip(*((int(n) for n in line.split()) for line in input)))
+    print(f"part1: {part1(first, second)}")
+    print(f"part2: {part2(first, second)}")
