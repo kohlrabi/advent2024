2147574d0b2d5d80120c8dbe8d0d95d891e42060
Jan Christoph Terasa <christoph@kohlio.de>
2024-12-02 18:21:55 +0100

    Solve day 2

---
+++ python/day02.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import itertools
+import operator
+from collections.abc import Sequence
+from itertools import pairwise
+
+from puzzle_input_getter import get_puzzle_input
+
+
+def is_sorted(report: Sequence[int]) -> bool:
+    """Returns True if report is sorted in increasing OR decreasing order"""
+    if len(report) <= 2:
+        return True
+    op = operator.gt if report[0] < report[1] else operator.lt
+
+    for x, y in pairwise(report):
+        if op(x, y):
+            return False
+    return True
+
+
+def is_safe(report: Sequence[int]) -> bool:
+    if is_sorted(report):
+        for x, y in pairwise(report):
+            if not (1 <= abs(x - y) <= 3):
+                break
+        else:
+            return True
+    return False
+
+
+def part1(reports: Sequence[Sequence[int]]) -> int:
+    total = sum(is_safe(report) for report in reports)
+    return total
+
+
+def part2(reports: Sequence[Sequence[int]]) -> int:
+    total = sum(
+        any(
+            is_safe(combination)
+            for combination in itertools.combinations(report, len(report) - 1)
+        )
+        for report in reports
+    )
+    return total
+
+
+def main() -> None:
+    input = get_puzzle_input(year=2024, day=2)
+    reports = [[int(x) for x in line.split()] for line in input]
+    print(f"part1: {part1(reports)}")
+    print(f"part2: {part2(reports)}")
+
+
+if __name__ == "__main__":
+    main()
