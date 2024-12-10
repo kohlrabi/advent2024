738aabd320cb9d6cb3bc3c28c479f4b2ecfd6ffd
Jan Christoph Terasa <christoph@kohlio.de>
2024-12-10 23:34:41 +0100

    Solve day 9, part 2

--- python/day09.py
+++ python/day09.py
@@ -43,8 +43,42 @@ def part1(filesystem: list[Entry]) -> int:
     return total
 
 
-def part2() -> int:
-    return 0
+def part2(filesystem: list[Entry]) -> int:
+    i = len(filesystem) - 1
+    moved = set()
+    while i > 0:
+        # for i in range(-1, -len(filesystem) - 1):
+        current = filesystem[i]
+        if current.id is None or current.id in moved:
+            i -= 1
+            continue
+
+        moved.add(current.id)
+
+        for j, entry in enumerate(filesystem):
+            if j >= i:
+                break
+            if entry.id is None and current.id and entry.length >= current.length:
+                remainder = entry.length - current.length
+                entry.id = current.id
+                entry.length = current.length
+                current.id = None
+                if remainder:
+                    filesystem.insert(j + 1, Entry(None, remainder))
+                    i += 1
+                break
+
+        i -= 1
+
+    pos = 0
+    total = 0
+    for entry in filesystem:
+        if entry.id is None:
+            pos += entry.length
+            continue
+        total += entry.id * sum(range(0 + pos, entry.length + pos))
+        pos += entry.length
+    return total
 
 
 def main() -> None:
@@ -63,7 +97,7 @@ def main() -> None:
             file = True
 
     print(f"part1: {part1(deepcopy(filesystem))}")
-    print(f"part2: {part2()}")
+    print(f"part2: {part2(deepcopy(filesystem))}")
 
 
 if __name__ == "__main__":
